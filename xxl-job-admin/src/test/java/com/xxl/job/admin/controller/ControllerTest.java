0ab036e4196bd7b2053aaa5b7bba2569aef575a2
zhenhc <zhenhaochuang@gmail.com>
2022-04-09 14:21:03 +0800

    add and update

--- xxl-job-admin/src/test/java/com/xxl/job/admin/controller/ControllerTest.java
+++ xxl-job-admin/src/test/java/com/xxl/job/admin/controller/ControllerTest.java
@@ -1,17 +1,27 @@
 package com.xxl.job.admin.controller;
 
+import cn.hutool.cache.Cache;
+import cn.hutool.cache.CacheUtil;
+import cn.hutool.captcha.CaptchaUtil;
+import cn.hutool.captcha.LineCaptcha;
 import cn.hutool.core.date.DateField;
 import cn.hutool.core.date.DateTime;
+import cn.hutool.core.date.DateUnit;
+import cn.hutool.core.io.FastByteArrayOutputStream;
 import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.io.IoUtil;
+import cn.hutool.core.io.file.PathUtil;
 import cn.hutool.core.io.resource.Resource;
 import cn.hutool.core.io.resource.ResourceUtil;
-import cn.hutool.core.util.ArrayUtil;
-import cn.hutool.core.util.RandomUtil;
-import cn.hutool.core.util.ReflectUtil;
+import cn.hutool.core.lang.Console;
+import cn.hutool.core.swing.clipboard.ClipboardUtil;
+import cn.hutool.core.util.*;
 import cn.hutool.http.HttpRequest;
+import cn.hutool.http.HttpUtil;
 import cn.hutool.json.JSONArray;
 import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
+import cn.hutool.log.Log;
 import com.xxl.job.admin.core.model.XxlJobInfo;
 import com.xxl.job.admin.service.LoginService;
 import lombok.extern.slf4j.Slf4j;
@@ -19,15 +29,16 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import redis.clients.jedis.Jedis;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
+import java.awt.datatransfer.Clipboard;
+import java.io.*;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.nio.charset.Charset;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @author : zhenhc
@@ -171,6 +182,78 @@ public class ControllerTest {
         //System.out.println(collect);
 
         List<String> fieldList = com.xxl.job.admin.core.util.ReflectUtil.getFieldList(XxlJobInfo.class,"xji");
-        System.out.println(fieldList);
+        //System.out.println(fieldList);
+
+        String content = "ZZZaaabbbccc中文1234";
+        List<String> resultFindAll = ReUtil.findAll("\\w{2}", content, 0, new ArrayList<String>());
+        //System.out.println(resultFindAll);
+
+        Set<Class<?>> classes = ClassUtil.scanPackage("cn.hutool");
+        Map<String, Object> stringObjectHashMap = new HashMap<>();
+
+        Map<String, Object> map1 = classes.stream()
+                .filter(aClass -> aClass.getName().endsWith("Util"))
+                .map(aClass -> {
+                    String name = aClass.getName();
+                    List<String> fieldList1 = com.xxl.job.admin.core.util.ReflectUtil.getFieldList(name.getClass());
+                    Method[] methods = ReflectUtil.getMethods(name.getClass());
+                    List<String> methodList = Arrays.stream(methods).map(method -> method.getName())
+                            .collect(Collectors.toList());
+                    Map<String, List<String>> stringListMap = new HashMap<>();
+                    stringListMap.put("fieldList", fieldList1);
+                    stringListMap.put("methodList", methodList);
+                    stringObjectHashMap.put(name, stringListMap);
+                    return stringObjectHashMap;
+                })
+                .collect(Collectors.toList()).get(0);
+        Map<String,Object> map = new HashMap<>();
+        map.put("Classs",map1);
+        map.put("count",map1.size());
+        System.out.println(JSONUtil.toJsonStr(map));
+    }
+
+    @Test
+    public void test4(){
+        //请求列表页
+        String listContent = HttpUtil.get("https://www.oschina.net/action/ajax/get_more_news_list?newsType=&p=2");
+        System.out.println(listContent);
+        //使用正则获取所有标题
+        List<String> titles = ReUtil.findAll("<span class=\"text-ellipsis\">(.*?)</span>", listContent, 1);
+        for (String title : titles) {
+            //打印标题
+            Console.log(title);
+        }
+    }
+
+    @Test
+    public void test5(){
+        List<File> files = FileUtil.loopFiles(new File("D:\\developer"), 3, new FileFilter() {
+            @Override
+            public boolean accept(File pathname) {
+                return true;
+            }
+        });
+        System.out.println(JSONUtil.toJsonStr(files));
+    }
+
+    @Test
+    public void test6(){
+//定义图形验证码的长和宽
+        LineCaptcha lineCaptcha = CaptchaUtil.createLineCaptcha(200, 100);
+
+//图形验证码写出，可以写出到文件，也可以写出到流
+        lineCaptcha.write("E:/line.png");
+//输出code
+        Console.log(lineCaptcha.getCode());
+//验证图形验证码的有效性，返回boolean值
+        lineCaptcha.verify("1234");
+
+//重新生成验证码
+        lineCaptcha.createCode();
+        lineCaptcha.write("E:/line.png");
+//新的验证码
+        Console.log(lineCaptcha.getCode());
+//验证图形验证码的有效性，返回boolean值
+        lineCaptcha.verify("1234");
     }
 }
